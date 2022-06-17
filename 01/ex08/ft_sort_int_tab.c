71d9523244b8a09b67dc7fe0ac12f5082966913f
amay5267 <amay5267@gmail.com>
2022-06-17 20:30:28 +0200

    Create ft_sort_int_tab.c

---
+++ 01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   xxxx.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xxxxx <xxxxxx@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: xxxx/xx/xx xx:xx:xx by xxxxx             #+#    #+#             */
+/*   Updated: xxxx/xx/xx xx:xx:xx by xxxxx            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int s;
+	int swap;
+
+	i = 0;
+	while (i < size)
+	{
+		s = 0;
+		while (s < (size - 1))
+		{
+			if (*(tab + i) < *(tab + s))
+			{
+				swap = *(tab + i);
+				*(tab + i) = *(tab + s);
+				*(tab + s) = swap;
+			}
+			s++;
+		}
+		i++;
+	}
+}
