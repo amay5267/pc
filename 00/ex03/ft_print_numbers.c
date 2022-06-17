56ad3be8b59ececaea9d977bc9282b853dfcae7a
amay5267 <amay5267@gmail.com>
2022-06-17 10:36:20 +0200

    Create ft_print_numbers.c

---
+++ 00/ex03/ft_print_numbers.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   xxxx.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xxxxx <xxxxxx@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: xxxx/xx/xx xx:xx:xx by xxxxx             #+#    #+#             */
+/*   Updated: xxxx/xx/xx xx:xx:xx by xxxxx            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void     ft_print_numbers(void)
+{
+         char number;
+    
+         number = '0';
+         while (number <= '9')
+         {
+         write(1, &number, 1);
+         number++;
+         }
+}
