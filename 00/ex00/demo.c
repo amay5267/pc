06cfeb841edb20d5f5c36d527d20c7aef127f30e
amay5267 <amay5267@gmail.com>
2022-06-17 10:07:35 +0200

    Create demo.c

---
+++ 00/ex00/demo.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+void     ft_putchar(char c);
+void     ft_putchar(char c)
+{
+         write(1, &c, 1);
+}
+
+int      main(void)
+{
+ft_putchar(';');
+ft_putchar(')');
+ft_putchar('\n');
+         return (0);
+}
