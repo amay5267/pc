95b19e34f8bfce7abdb36a96ffbd3c7c21bd1b63
amay5267 <amay5267@gmail.com>
2022-06-17 18:54:48 +0200

    Create demo.c

---
+++ 00/ex07/demo.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	num;
+
+	if (nb < 0)
+	{
+		num = (unsigned int)(-1 * nb);
+		ft_putchar('-');
+	}
+	else
+	{
+		num = (unsigned int)(nb);
+	}
+	if (num >= 10)
+	{
+		ft_putnbr(num / 10);
+		ft_putnbr(num % 10);
+	}
+	else
+	{
+		ft_putchar(num + '0');
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(7);
+}
