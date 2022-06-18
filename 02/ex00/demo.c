a67fa61af78b102dd74294e0a4058e4bc950d07a
amay5267 <amay5267@gmail.com>
2022-06-18 13:40:22 +0200

    Update demo.c

--- 02/ex00/demo.c
+++ 02/ex00/demo.c
@@ -1,20 +1,41 @@
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	write(1, str, i);
+}
+
 char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i])
 	{
-		dest[i] = src[i];
-		i++;
+	dest[i] = src[i];
+	i++;
 	}
-	dest[i] = '\0';
+	dest[i] = src[i];
 	return (dest);
 }
 
 int	main(void)
 {
+	char	r[] = {"Launch"};
+	char	t[7];
+	char	*p;
 
+	p = ft_strcpy(t, r);
+	ft_putstr(p);
+	return (0);
 }
